090e6938d97d365cfaab544a1564255211ec9f36
PamelaVeraCASTRO <92119095+PamelaVeraCASTRO@users.noreply.github.com>
2023-04-28 16:13:19 -0500

    Delete index.js

--- primer parc8al/1erPractica/index.js
+++
@@ -1,8 +0,0 @@
-//Declaramos la importación de lo que vamos a proceder a imprimir o usar.
-const {mostrarRegistrodepalabras, mostrarDescripcion,mostraridiomayregistro, registro, palabra, idioma} = require('./entidadesyfunciones')
-// FUNCIONES LLAMADAS 
-mostrarRegistrodepalabras(palabra,registro)
-mostrarDescripcion(registro,idioma,palabra)
-mostraridiomayregistro(idioma, registro, palabra)
-
-
